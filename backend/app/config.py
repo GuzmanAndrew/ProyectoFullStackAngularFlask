f188fdf2068f38ac08e8652b19afcf9a15a95d67
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2024-08-31 07:50:22 -0500

    final proyect connect db aws and atlasian

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'MYSQL_DATABASE_URI', 'mysql+pymysql://root:Andrewseigokan9*@localhost/inventario_db'
+        'MYSQL_DATABASE_URI', 'mysql+pymysql://admin:Andrewseigokan9*@product-db.c9kkkoim4kom.us-east-1.rds.amazonaws.com:3306/inventario_db'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/inventario_db')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://ramirezandrew172:G3FWuPqdVfnTVX43@inventario-clu.jnk1e.mongodb.net/inventario_db?retryWrites=true&w=majority&appName=inventario-clu')
